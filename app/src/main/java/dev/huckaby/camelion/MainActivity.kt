2e98ee98c301005f51d3f9d6ff53c7740cdea0c8
Daniel Huckaby <handlerexploit@gmail.com>
2024-08-25 13:19:55 -0500

    Display both xml and compose

--- app/src/main/java/dev/huckaby/camelion/MainActivity.kt
+++ app/src/main/java/dev/huckaby/camelion/MainActivity.kt
@@ -10,12 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dev.huckaby.camelion.databinding.MainBinding
 import dev.huckaby.camelion.ui.theme.CamelionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        val binding = MainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.greetings.text = "Hello Android!"
+
+        binding.compose.setContent {
             CamelionTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
--- app/src/main/java/dev/huckaby/camelion/ui/theme/Theme.kt
+++ app/src/main/java/dev/huckaby/camelion/ui/theme/Theme.kt
@@ -2,10 +2,7 @@ package dev.huckaby.camelion.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -15,11 +12,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
@@ -37,7 +29,6 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun CamelionTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -45,10 +36,8 @@ fun CamelionTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
@@ -56,7 +45,7 @@ fun CamelionTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
